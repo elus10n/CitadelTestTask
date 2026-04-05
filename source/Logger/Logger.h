e9461f5bc99a6a8b35880025fab7598eb9727208
Иван Лукин <dizbvalkyre@gmail.com>
2026-04-05 18:03:06 +0300

    simple logger

--- source/Logger/Logger.h
+++ source/Logger/Logger.h
@@ -1,6 +1,20 @@
 #ifndef LOGGER_H
 #define LOGGER_H
 
+#include <iostream>
 
+#include "../../include/models.h"
+
+namespace Logger
+{
+    LogCallback getCallback()
+    {
+        auto callback = [](const std::string& error)
+        {
+            std::cerr << error << std::endl;
+        };
+        return callback;
+    }
+}
 
 #endif
