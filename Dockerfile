b58bd86374b78a52ba742b86b9ef53dee76e4c03
Иван Лукин <dizbvalkyre@gmail.com>
2026-04-06 19:08:51 +0300

    docker

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM gcc:latest
+
+RUN apt-get update && apt-get install -y cmake
+
+WORKDIR /usr/src/citadel
+
+COPY . .
+
+RUN mkdir build && cd build && \
+    cmake .. && \
+    cmake --build .
+
+CMD cd build && ./unit_tests && ./main_app
---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include "source/orchestrator/orchestrator.h"
+#include "source/logger/logger.h"
+#include <iostream>
+#include <vector>
+#include <filesystem>
+
+int main(int argc, char** argv) 
+{
+    setlocale(LC_ALL, "ru_RU.UTF-8");
+
+    std::string configPath = (argc > 1) ? argv[1] : "example/example.json";
+    std::string dataDir = (argc > 2) ? argv[2] : "example/data/";
+    std::string reportPath = "example/report.txt";
+
+    std::vector<std::string> filePaths;
+    try 
+    {
+        for (const auto& entry : std::filesystem::directory_iterator(dataDir)) 
+            if (entry.is_regular_file()) 
+                filePaths.push_back(entry.path().string());
+    } 
+    catch (const std::exception& e) 
+    {
+        std::cerr << "Error reading directory: " << e.what() << std::endl;
+        return 1;
+    }
+
+    Orchestrator orch;
+    orch.setCallback(Logger::getCallback(std::cerr));
+    
+    orch.launchParser(configPath, filePaths, reportPath);
+
+    return 0;
+}
